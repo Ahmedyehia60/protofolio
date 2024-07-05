4d2d05ba21668890fea1e87bd91ac5f2c35fdeab
Ahmed yehia <ahmedyehia2715@gmail.com>
2024-07-05 21:48:53 +0300

    Add files via upload

---
+++ porto.js
@@ -0,0 +1,12 @@
+let menuIcon = document.querySelector('#menue-icon');
+let navbar = document.querySelector('.navbar');
+
+
+menuIcon.onclick =()=>{
+    menuIcon.classList.toggle('fa-xmark');
+    navbar.classList.toggle('active');
+}
+
+/*------------------scroll section----------------  */
+
+
